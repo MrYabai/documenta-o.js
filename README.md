f596bb06d05434c1c034b85b03480ca7e832ab17
MrYabai <graovizir0090@gmail.com>
2024-09-11 15:25:50 -0300

    release 1.1.1
    mudanças e declaração de variáveis completas

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 **javascript**
 <p>aqui eu farei uma documentação completa do javascript de uma forma que eu consiga aprender e talvez ate ensinar de forma mais facil tambem espero poder ajudar as pessoas que estão com dificuldade no aprendizado</p>
 
+e recomendado seguir a ordem dos materiais
---
+++ basic/2_!_syntaxe-basica/1_declarar-variaveis.js
@@ -0,0 +1,22 @@
+//ja que entendem oque e um comentario a partir de agora tudo sera feitos nos arquivos js
+
+/*
+em javascript, usamos variaveis para armazenar dados
+
+    var : var e a forma antiga de declarar variaveis. o scopo pode ser "global" ou "local a uma função"
+
+    let : Introduzida no ECMAScript 6 (ES6), permite declarar variáveis com escopo de bloco, ou seja, só existem dentro de um bloco específico ({}). Usada para variáveis que podem mudar.
+
+    const : Também introduzida no ES6, é usada para variáveis que não podem ser reatribuídas.
+
+*/
+
+//exemplos
+var nome = "João";
+// primeiro a palavra chave da variavel e depois o nome dela e então um "="(no js nos chamamso o "=" sozinho de recebe) e então o valor da variavel
+
+let nome = 30;
+// basicamente a mesma syntax do "var" as vareaveis podem receber diferentes tipos de "valores" (veremso isso mais pra frente)
+
+const pais = "Brasil";
+// mesma coisa das outras a diferença e que uma "const" não pode ser modificada (veremos isso mais para frente)
